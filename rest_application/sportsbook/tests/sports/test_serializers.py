9a710f1948321b2785e4c46378c2ad4e74cf1f13
Joao Vitor <vitor.gomessanto@gmail.com>
2021-12-16 18:07:18 -0300

    improve tests

--- rest_application/sportsbook/tests/sports/test_serializers.py
+++ rest_application/sportsbook/tests/sports/test_serializers.py
@@ -10,8 +10,18 @@ def create_sports_serializer_fixture():
 
 
 @pytest.fixture
+def create_sports_serializer_error_fixture():
+    return QueryDict('name=&active=true')
+
+
+@pytest.fixture
 def update_sports_serializer_fixture():
     return QueryDict('id=1&name=Golf&active=null')
+
+
+@pytest.fixture
+def update_sports_serializer_error_fixture():
+    return QueryDict('id=&name=Golf&active=null')
 
 
 def test_create_sports_serializer(create_sports_serializer_fixture):
@@ -20,7 +30,19 @@ def test_create_sports_serializer(create_sports_serializer_fixture):
     assert serializer.data == {'name': 'Soccer', 'slug': 'soccer', 'active': True}
 
 
+def test_create_sports_serializer_error(create_sports_serializer_error_fixture):
+    serializer = CreateSportsSerializer(data=create_sports_serializer_error_fixture)
+    serializer.is_valid()
+    assert str(serializer.errors['name'][0]) == 'This field may not be blank.'
+
+
 def test_update_sports_serializer(update_sports_serializer_fixture):
     serializer = UpdateSportsSerializer(data=update_sports_serializer_fixture)
     serializer.is_valid()
     assert serializer.data == {'id': 1, 'name': 'Golf', 'slug': 'golf'}
+
+
+def test_update_sports_serializer_error(update_sports_serializer_error_fixture):
+    serializer = UpdateSportsSerializer(data=update_sports_serializer_error_fixture)
+    serializer.is_valid()
+    assert str(serializer.errors['id'][0]) == 'A valid integer is required.'
